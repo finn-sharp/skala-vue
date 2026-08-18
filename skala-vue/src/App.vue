ac358c3557dd734caf0c6d5eca00dc4f6cc2b2fc
finn-sharp <penguin-klg@naver.com>
2026-08-18 16:11:53 +0900

    Code Cahllenge : ref data & Text interpolartion Practice

---
+++ skala-vue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SampleOne from './components/SampleOne.vue'
+import SampleTwo from './components/SampleTwo.vue'
+
+</script>
+
+<template>
+  <div>
+    <SampleOne/>
+    <SampleTwo/>
+  </div>
+</template>
+
+<style scoped>
+div {
+  padding:20px
+}
+</style>
---
+++ skala-vue/src/components/SampleOne.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+
+let normalCount=0 // 값(Primitive)
+const vueCount = ref(0) // Binding 시킴(반응형 변수)
+</script>
+
+<template>
+    <div class="practice-section">
+        <h3>일반 변수 클릭 : {{ normalCount }}</h3>
+        <button @click="normalCount++"></button>
+        <br>
+        <h3>Vue 반응성 변수 클릭 : {{ vueCount }}</h3>
+        <button @click="vueCount++"></button>
+    </div>
+</template>
+
