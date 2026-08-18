3729799eb2c0df093f9207b0418f37140adb9e8c
finn-sharp <penguin-klg@naver.com>
2026-08-18 16:15:06 +0900

    Code Cahllenge : btn 변수명 추가

--- skala-vue/src/components/SampleOne.vue
+++ skala-vue/src/components/SampleOne.vue
@@ -8,10 +8,10 @@ const vueCount = ref(0) // Binding 시킴(반응형 변수)
 <template>
     <div class="practice-section">
         <h3>일반 변수 클릭 : {{ normalCount }}</h3>
-        <button @click="normalCount++"></button>
-        <br>
+        <button @click="normalCount++">일반 변수 증가</button>
+        <br/>
         <h3>Vue 반응성 변수 클릭 : {{ vueCount }}</h3>
-        <button @click="vueCount++"></button>
+        <button @click="vueCount++">Vue 변수 증가</button>
     </div>
 </template>
 
