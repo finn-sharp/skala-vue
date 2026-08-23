16365ac9c887af20797e4ba0050bb85586264069
finn-sharp <penguin-klg@naver.com>
2026-08-23 20:40:14 +0900

    Subject : Nasted Router를 활용하여 과제 내부 component 구성

---
+++ skala-vue/src/components/subject/weatherRouter/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import UnitToggler from '../weatherStore/UnitToggler.vue';
+</script>
+
+<template>
+  <div class="app-container">
+    <div class="dashboard-wrapper">
+      <nav class="navigation-bar">
+        <RouterLink to="/detail" class="nav-item">🌦️ 날씨 대시보드</RouterLink>
+        <span class="divider"> | </span>
+        <RouterLink to="/detail/about" class="nav-item">ℹ️ 서비스 소개</RouterLink>
+        <UnitToggler />
+      </nav>
+      <br>
+      <main>
+        <RouterView />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import '@/assets/style.css';
+.app-container {
+  max-width: 1440px;
+  margin-inline: auto;
+}
+</style>
